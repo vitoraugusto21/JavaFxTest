3903280578c3a81570ea85eb8e1690b1468a20ba
Vítor Augusto <vitoraug210@gmail.com>
2023-05-25 14:16:09 -0300

    Criação do teste

---
+++ JavaFxTest/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.javafxtest {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.javafxtest to javafx.fxml;
+    exports com.example.javafxtest;
+}
