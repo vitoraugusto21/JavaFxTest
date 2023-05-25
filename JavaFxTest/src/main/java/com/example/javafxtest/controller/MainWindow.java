34eec68308c4ad345b3798a9dc040e803735a986
Vítor Augusto <vitoraug210@gmail.com>
2023-05-25 14:42:44 -0300

    Update MainWindow.java
    substitui o controller

--- JavaFxTest/src/main/java/com/example/javafxtest/controller/MainWindow.java
+++ JavaFxTest/src/main/java/com/example/javafxtest/controller/MainWindow.java
@@ -1,14 +1,27 @@
 package com.example.javafxtest.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class MainWindow {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button attendant;
+
+    @FXML
+    private Button enter;
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private Button manager;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button tecnichian;
+
 }
