e6deb89554db8422c9efe546589fd72124590a94
Vítor Augusto <vitoraug210@gmail.com>
2023-06-08 20:16:04 -0300

    Create AttendantWindow

---
+++ JavaFxTest/src/main/java/com/example/javafxtest/controller/AttendantWindow.java
@@ -0,0 +1,41 @@
+package com.example.javafxtest.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AttendantWindow {
+
+    @FXML
+    private Button CancelOs;
+
+    @FXML
+    private Button EditClient;
+
+    @FXML
+    private Button Payment;
+
+    @FXML
+    private Button RegisterClient;
+
+    @FXML
+    void CancelOsAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EditClientAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PaymentAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void RegisterClientAction(ActionEvent event) {
+
+    }
+
+}
--- JavaFxTest/src/main/java/com/example/javafxtest/controller/MainWindow.java
+++ JavaFxTest/src/main/java/com/example/javafxtest/controller/MainWindow.java
@@ -3,7 +3,6 @@ package com.example.javafxtest.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 
 public class MainWindow {
 
@@ -11,19 +10,11 @@ public class MainWindow {
     private Button BtnAttendant;
 
     @FXML
-    private Button enter;
-
-    @FXML
-    private TextField id;
-
-    @FXML
     private Button manager;
 
     @FXML
-    private TextField password;
-
-    @FXML
     private Button tecnichian;
+
 
     @FXML
     void BtnAttendantAction(ActionEvent event) {
