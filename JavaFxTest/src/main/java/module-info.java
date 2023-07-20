c25a919bbb098a2a745199a4f2d122c04950ba81
Vítor Augusto <vitoraug210@gmail.com>
2023-07-20 09:59:26 -0300

    Update module-info.java
    Estava com problemas para acessar os enums, consegui fazendo o exports pelo module info

--- JavaFxTest/src/main/java/module-info.java
+++ JavaFxTest/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.example.javafxtest {
     opens com.example.javafxtest to javafx.fxml;
     exports com.example.javafxtest;
     exports com.example.javafxtest.controller;
+    exports com.example.javafxtest.model.entities.enums to com.google.gson;
     opens com.example.javafxtest.controller to javafx.fxml;
     opens com.example.javafxtest.model.entities to com.google.gson;
     opens com.example.javafxtest.model.dao to com.google.gson;
