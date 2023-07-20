632adcd70f825d895a0d5374ccbbcb0dbf3b5e6f
Vítor Augusto <vitoraug210@gmail.com>
2023-07-20 11:14:32 -0300

    Create EditClientWindow.java
    Criei a tela de editar cliente

---
+++ JavaFxTest/src/main/java/com/example/javafxtest/controller/EditClientWindow.java
@@ -0,0 +1,22 @@
+package com.example.javafxtest.controller;
+
+import com.example.javafxtest.MainApplication;
+import javafx.event.ActionEvent;
+
+public class EditClientWindow {
+    public void ReturnAction(ActionEvent actionEvent) {
+        MainApplication.changeScreen("attendant");
+    }
+
+    public void EditNameAction(ActionEvent actionEvent) {
+    }
+
+    public void editPhoneNumberAction(ActionEvent actionEvent) {
+    }
+
+    public void editEmailAction(ActionEvent actionEvent) {
+    }
+
+    public void editAddressAction(ActionEvent actionEvent) {
+    }
+}
