2b84182c14e8e8208ca93c418850df824cb55360
Vítor Augusto <vitoraug210@gmail.com>
2023-08-03 21:14:04 -0300

    Update PaymentWindow.java
    Adicionei a comboBox

--- JavaFxTest/src/main/java/com/example/javafxtest/controller/PaymentWindow.java
+++ JavaFxTest/src/main/java/com/example/javafxtest/controller/PaymentWindow.java
@@ -1,34 +1,57 @@
 package com.example.javafxtest.controller;
 
-import com.example.javafxtest.MainApplication;
-import com.example.javafxtest.model.dao.OsDAOImp;
-import com.example.javafxtest.model.dao.TechnicianDAOImp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
-import java.io.File;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 public class PaymentWindow {
-    OsDAOImp osDAOImp = new OsDAOImp();
-    TechnicianDAOImp technicianDAOImp = new TechnicianDAOImp();
-    File file = new File("os.json");
 
+    @FXML
+    private Button Return;
 
+    @FXML
+    private Button cash;
 
-    public void cashAction(ActionEvent actionEvent) {
+    @FXML
+    private TextField clienteCpf;
+
+    @FXML
+    private ComboBox<?> comboBox;
+
+    @FXML
+    private Button credit;
+
+    @FXML
+    private Button debitAction;
+
+    @FXML
+    private Button pix;
+
+    @FXML
+    void CreditAction(ActionEvent event) {
 
     }
 
-    public void debitAction(ActionEvent actionEvent) {
+    @FXML
+    void PixAction(ActionEvent event) {
+
     }
 
-    public void PixAction(ActionEvent actionEvent) {
-    }
-
-    public void CreditAction(ActionEvent actionEvent) {
-    }
     @FXML
     void ReturnAction(ActionEvent event) {
-        MainApplication.changeScreen("attendant");
+
     }
+
+    @FXML
+    void cashAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void debitAction(ActionEvent event) {
+
+    }
+
 }
