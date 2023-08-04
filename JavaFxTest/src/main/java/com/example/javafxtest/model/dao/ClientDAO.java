3794e55223e7ba249c555d2eaa9e6c29640d204b
Vítor Augusto <vitoraug210@gmail.com>
2023-08-03 21:20:55 -0300

    Update ClientDAO.java

--- JavaFxTest/src/main/java/com/example/javafxtest/model/dao/ClientDAO.java
+++ JavaFxTest/src/main/java/com/example/javafxtest/model/dao/ClientDAO.java
@@ -1,6 +1,7 @@
 package com.example.javafxtest.model.dao;
 
 import com.example.javafxtest.model.entities.Client;
+import com.example.javafxtest.model.entities.Os;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,8 +11,7 @@ import java.util.Map;
  * Interface responsável por definir as operações de acesso aos dados de Cliente no banco de dados.
  */
 public interface ClientDAO {
-
-    //para ser considerado crud, é necessário todos esses métodos.
+    void insertOsInCliente(Os os, String clientCpf) throws IOException;
     void createClient(Client client) throws IOException;
 
     void updateClient(Client client, String attributeToChange, String newAttribute) throws IOException;
