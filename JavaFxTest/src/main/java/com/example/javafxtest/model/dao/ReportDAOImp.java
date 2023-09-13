763adedeb3d223d5ef6d23e3cec949dd83809b46
Vítor Augusto <vitoraug210@gmail.com>
2023-09-13 14:53:36 -0300

    Update ReportDAOImp.java
    Atualizei a documentação do codigo.

--- JavaFxTest/src/main/java/com/example/javafxtest/model/dao/ReportDAOImp.java
+++ JavaFxTest/src/main/java/com/example/javafxtest/model/dao/ReportDAOImp.java
@@ -9,10 +9,24 @@ import java.util.Date;
 
 public class ReportDAOImp implements ReportDAO {
 
+    /**
+            * Gera um nome de arquivo com base no nome do relatório e na data atual.
+     *
+             * @param report O relatório para o qual o nome do arquivo será gerado.
+     * @return O nome do arquivo gerado.
+            */
     public String getFilename(Report report) {
         var now = new Date();
         return "report-" + report.getName() + "__" + now.getYear()+now.getMonth()+now.getDay() + ".txt";
     }
+
+    /**
+     * Salva um relatório em um arquivo de texto.
+     *
+     * @param report O relatório a ser salvo.
+     * @throws Exception               Se ocorrer um erro ao criar ou escrever no arquivo.
+     * @throws UnsupportedEncodingException Se ocorrer um erro de codificação não suportada.
+     */
 
     @Override
     public void saveReport(Report report) throws Exception, UnsupportedEncodingException {
@@ -22,15 +36,3 @@ public class ReportDAOImp implements ReportDAO {
     }
 
 }
-
-/**
- * public String toString() {
- * return "Fatura: " +
- * "Cliente: " + client.getName() +
- * ", Tecnico: " + technician.getName() +
- * ", Atendente: " + attendant.getName() +
- * ", Valor Total: " + os.getTotalValue() +
- * ", Forma de pagamento: " + payment +
- * os.getDescription();
- * }
- */
